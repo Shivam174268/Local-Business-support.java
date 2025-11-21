94e115449d3d2ed5e31f92943ff22b0812bb7060
Shivam174268 <suryakumar9932@gmail.com>
2025-11-21 17:02:23 +0530

    Create ServiceInterface as a marker interface

---
+++ ServiceInterface.java
@@ -0,0 +1,8 @@
+package com.localbiz.service;
+
+/**
+ * Marker interface for all services.
+ */
+public interface ServiceInterface {
+    // Can add default methods for common service operations if needed
+}
