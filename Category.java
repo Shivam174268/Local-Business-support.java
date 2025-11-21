0d92a27fd73823aa1977a6ed8bc04a38bc139991
Shivam174268 <suryakumar9932@gmail.com>
2025-11-21 16:55:18 +0530

    Add Category class to represent business categories

---
+++ Category.java
@@ -0,0 +1,13 @@
+package com.localbiz.model;
+
+/**
+ * Represents a Business category.
+ */
+public class Category {
+    private String name;
+
+    public Category(String name) { this.name = name; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+}
